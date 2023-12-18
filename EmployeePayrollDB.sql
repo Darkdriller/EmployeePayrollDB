c77c3eadbb472373e1e970234bdfd703ccf4ff12
~Exypnos <83203139+Darkdriller@users.noreply.github.com>
2023-12-18 10:56:47 +0530

    Create EmployeePayrollDB.sql

---
+++ EmployeePayrollDB.sql
@@ -0,0 +1,24 @@
+/* UC-1 */
+
+
+mysql> CREATE database employee_payroll_service;
+Query OK, 1 row affected (0.01 sec)
+
+mysql> SHOW DATABASES;
++--------------------------+
+| Database                 |
++--------------------------+
+| address_book_system      |
+| employee_payroll_service |
+| information_schema       |
+| mysql                    |
+| performance_schema       |
+| sakila                   |
+| sys                      |
++--------------------------+
+7 rows in set (0.02 sec)
+
+mysql> USE employee_payroll_service;
+Database changed
+
+
